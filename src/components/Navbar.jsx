40fb4c65893811285162e2bbff4201a888f3c4e2
faisalhrbk <faisalhrbk@outlook.com>
2025-06-02 01:12:53 +0500

    Initial commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -75,7 +75,7 @@ const NavBar = () => {
 
             <Button
               id="product-button"
-              title="P roducts"
+              title="Products"
               rightIcon={<TiLocationArrow />}
               containerClass="bg-blue-50 md:flex hidden items-center justify-center gap-1"
             />
